da843a61fbea9b4eb5d3acd40d98b91a8137cd77
Raish <temp45802@gmail.com>
2023-08-26 22:34:29 +0530

    Learned Literals in java

---
+++ src/com/company/CWH_04_Literals.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class CWH_04_Literals
+{
+    public static void main(String args[])
+    {
+        byte age = 34;
+        int a = 234;
+        long ageDino = 325434L;
+        char ch = 'a';
+        float f = 5.6F;
+        double d = 435.34523;
+        boolean isValid = true;
+        String str = "Raish";
+        System.out.println(age);
+        System.out.println(str);
+    }
+}
