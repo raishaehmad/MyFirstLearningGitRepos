29b31ac4b6b456925964ed570e37b159b69ed9f1
Raish <temp45802@gmail.com>
2023-08-26 21:19:56 +0530

    learned variables and data type implemented 3 decimal no. adding in java

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("The Sum of three number is: ");
+
+        // Quick Quiz Write a program to add three numbers.
+        int n1 = 5;
+        int n2 = 5;
+        int n3 = 5;
+        System.out.println(n1+n2+n3);
+    }
+}
+
+//add two numbers
+//AddTwoNumbers  ---> Pascal Case Convention --> used for class naming
+//addTwoNumber   ---> camel Case Convention  --> used for function naming
+
