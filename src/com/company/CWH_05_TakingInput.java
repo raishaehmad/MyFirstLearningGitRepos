98bdeac9a4cdb946069a2eb8e0585870166c71dd
Raish <temp45802@gmail.com>
2023-08-26 23:05:17 +0530

    Learned Taking Input from the user in java using scanner class

---
+++ src/com/company/CWH_05_TakingInput.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.util.Scanner;
+
+public class CWH_05_TakingInput
+{
+    public static void main(String[] args)
+    {
+        int a;
+        // float a;
+        int b;
+        // float b;
+        System.out.println("-------Taking Input-------");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number 1: ");
+        a = sc.nextInt();
+        //a = sc.nextFloat();
+        System.out.println("Enter Number 2: ");
+        b = sc.nextInt();
+        //b = sc.nextFloat();
+        int sum = a + b;
+        // float sum = a + b;
+        System.out.println("Sum of three number is: " + sum);
+        // or
+        //System.out.println("Sum of three number is: ");
+        //System.out.println(sum);
+        //boolean b1 = sc.hasNextInt();
+        //System.out.println(b1);
+        //String s = sc.next(); // this sc.next() reads only one word
+        String s = sc.nextLine(); // this reads line
+        System.out.println(s);
+    }
+}
