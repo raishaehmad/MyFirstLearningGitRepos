753077dc82dca4e4a2e57623391a321b072c01e8
Raish <temp45802@gmail.com>
2023-08-29 00:08:05 +0530

    Video 14 Learned string methods

---
+++ src/com/company/CWH_14_String_Methods.java
@@ -0,0 +1,52 @@
+package com.company;
+
+public class CWH_14_String_Methods
+{
+    public static void main(String[] args)
+    {
+        String name = "Raish";
+        //System.out.println(name);
+        //int value = name.length();
+        //System.out.println(value);
+
+        //String lwrname = name.toLowerCase();
+        //System.out.println(lwrname);
+
+        //String uprname = name.toUpperCase();
+        //System.out.println(uprname);
+
+        //String nonTrimmedString = "    Raish    ";
+        //System.out.println(nonTrimmedString);
+        //System.out.println(nonTrimmedString.trim());
+        //String  trimmedString = nonTrimmedString.trim();
+        //System.out.println(trimmedString);
+
+        //System.out.println(name.substring(2));
+        //System.out.println(name.substring(2, 4));
+
+        //System.out.println(name.replace('h' , 'e'));
+        //System.out.println("Harry".replace('r', 'p'));
+
+        //System.out.println("Harry".replace("r", "ier"));
+
+        //System.out.println(name.startsWith("Rai")); // returns boolean
+        //System.out.println(name.endsWith("sh")); // returns boolean
+
+        //System.out.println(name.charAt(2)); // what is the character at index 2
+
+        //System.out.println(name.indexOf('s')); // at the character s index is 3 in name
+
+        //String modifiedString = "Raishsh";
+        //System.out.println(modifiedString.indexOf("sh" , 4));
+        //System.out.println(modifiedString.indexOf("sh23" , 2)); // here sh23 not match and return -1
+        //System.out.println(modifiedString.lastIndexOf("sh")); // returns 5 because it reads from the last
+        //System.out.println(modifiedString.lastIndexOf("sh", 4)); // returns 3
+
+        //System.out.println(name.equals("Raish"));
+        //System.out.println(name.equalsIgnoreCase("raish"));
+
+        //--------------------escape sequence character
+
+        //System.out.println("this is 1 st line \n second line");
+    }
+}
