bfc1120d9fa33b48c18d887f8af2ab482d0ed8f9
Raish <temp45802@gmail.com>
2023-08-31 00:07:34 +0530

    Learned COnditionals if-else

---
+++ src/com/company/CWH_16_Conditionals.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class CWH_16_Conditionals
+{
+    public static void main(String[] args)
+    {
+        int age = 45;
+        if (age >= 18)
+        {
+            System.out.println("You Can Drive");
+        }
+        else
+        {
+            System.out.println("You Cannot Drive Below 18.");
+        }
+    }
+}
