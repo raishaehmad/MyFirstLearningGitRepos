9362f0b2fd32c046a8ce70dba4920c403c6337cf
Raish <temp45802@gmail.com>
2023-09-12 14:22:50 +0530

    While loops

---
+++ src/com/company/CWH_21_CH5_Lopps.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class CWH_21_CH5_Lopps
+{
+    public static void main(String[] args)
+    {
+        int i = 0;
+        while (i < 5)
+        {
+            System.out.println(i);
+            i++;
+        }
+
+        //while(true)
+        //{
+        //    System.out.println(i);
+        //}
+    }
+}
