f79662b125898e7bc3b157be937af17cb2a93b20
Raish <temp45802@gmail.com>
2023-09-16 22:53:38 +0530

    Arrays

---
+++ src/com/company/CWH_26_Arrays.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class CWH_26_Arrays
+{
+    public static void main(String[] args)
+    {
+        int [] a = new int[5];
+        a[0] = 234;
+        a[1] = 34;
+        a[2] = 356;
+        a[3] = 23;
+        a[4] = 87;
+        System.out.println(a[3]);
+
+        int [] _3rdWay = {1, 32, 234, 21, 34};
+        System.out.println(_3rdWay[2]);
+    }
+}
